ff3f5e589bae217618eaa4c4602787bf9447cad6
Porodog_ <jklasd147@naver.com>
2025-11-25 10:39:34 +0900

    Update Socket.ts

--- src/Socket.ts
+++ src/Socket.ts
@@ -1,3 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000");
+
+// export const socket = io("http://localhost:4000");
+
+export const socket = io("doodle-production.up.railway.app", {
+  transports: ["websocket"]
+});
